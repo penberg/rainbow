67542c168bea1754e8d121decf8403b77142584b
Pekka Enberg <penberg@iki.fi>
2019-02-24 13:50:41 +0200

    Move Reactor type to a header file

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 clang -I/home/penberg/linux/tools/lib/bpf -target bpf -c rainbow_pass_kern.c -o rainbow_pass_kern.o -O3
 clang -I/home/penberg/linux/tools/lib/bpf -target bpf -c rainbow_kern.c -o rainbow_kern.o -O3
-g++ -Wall -O3 -std=gnu++17 -Iinclude -I/home/penberg/linux/tools/lib/bpf rainbowd.cpp -o rainbowd -L/home/penberg/linux/tools/lib/bpf -l:libbpf.a -lelf
+g++ -Wall -O3 -std=gnu++17 -Iinclude -I. -I/home/penberg/linux/tools/lib/bpf rainbowd.cpp reactor.cpp -o rainbowd -L/home/penberg/linux/tools/lib/bpf -l:libbpf.a -lelf
---
+++ include/rainbow/reactor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "expected.hpp"
+
+#include <functional>
+
+namespace rainbow {
+
+struct Packet;
+
+using Error = std::string;
+
+using OnPacketFn = std::function<tl::expected<void, Error>(const Packet& packet)>;
+
+class Reactor {
+  OnPacketFn _fn;
+public:
+  void on_packet(OnPacketFn&& fn);
+  void run();
+};
+
+}
