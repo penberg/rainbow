3e2c40d1be19e358ebdd1c9c0077d58440a13fe4
Pekka Enberg <penberg@iki.fi>
2019-02-26 18:38:03 +0200

    build.sh: Add LIBBPF_PATH variable to the build script
    Add a LIBBPF_PATH variable to the build script to make it easier to
    configure libbpf path.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-clang -I/home/penberg/linux/tools/lib/bpf -target bpf -c rainbow_pass_kern.c -o rainbow_pass_kern.o -O3
-clang -I/home/penberg/linux/tools/lib/bpf -target bpf -c rainbow_kern.c -o rainbow_kern.o -O3
-g++ -Wall -O3 -std=gnu++17 -Iinclude -I. -I/home/penberg/linux/tools/lib/bpf rainbowd.cpp reactor.cpp -o rainbowd -L/home/penberg/linux/tools/lib/bpf -l:libbpf.a -lelf
+LIBBPF_PATH=/home/penberg/linux/tools/lib/bpf
+
+clang -I$LIBBPF_PATH -target bpf -c rainbow_pass_kern.c -o rainbow_pass_kern.o -O3
+clang -I$LIBBPF_PATH -target bpf -c rainbow_kern.c -o rainbow_kern.o -O3
+g++ -Wall -O3 -std=gnu++17 -Iinclude -I. -I$LIBBPF_PATH rainbowd.cpp reactor.cpp -o rainbowd -L$LIBBPF_PATH -l:libbpf.a -lelf
