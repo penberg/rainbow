60bae14d57926ceb6c5d277e427089faff98530b
Pekka Enberg <penberg@iki.fi>
2019-02-26 18:38:40 +0200

    build.sh: Build libbpf dependency
    Build libbpf dependency as part of the build to avoid requiring the user
    to do that manually.

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ LIBBPF_PATH=/home/penberg/linux/tools/lib/bpf
 
 clang -I$LIBBPF_PATH -target bpf -c rainbow_pass_kern.c -o rainbow_pass_kern.o -O3
 clang -I$LIBBPF_PATH -target bpf -c rainbow_kern.c -o rainbow_kern.o -O3
+make -C $LIBBPF_PATH all
 g++ -Wall -O3 -std=gnu++17 -Iinclude -I. -I$LIBBPF_PATH rainbowd.cpp reactor.cpp -o rainbowd -L$LIBBPF_PATH -l:libbpf.a -lelf
