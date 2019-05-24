f1e53110f39023c93ae9955ce0dd88b4296b7785
Pekka Enberg <penberg@iki.fi>
2019-05-24 08:43:38 +0300

    install-deps: Add support for Ubuntu

--- install-deps.sh
+++ install-deps.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-dnf -y install make clang llvm gcc-c++ elfutils-devel
+set -e
+
+. /etc/os-release
+
+if [ "$ID" = "fedora" ]; then
+    sudo dnf -y install make clang llvm gcc-c++ elfutils-devel
+elif [ "$ID" = "ubuntu" ]; then
+    sudo apt install --yes clang g++ linux-libc-dev libelf-dev
+else
+    echo "Warning: '$ID' is not a supported OS."
+fi
